fc48910d1563d7e7cd0858c0f909e518af538137
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2021-12-23 12:27:31 -0800

    initial commit

---
+++ Inheritance/Animal1.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritance
+{
+    internal class Animal
+    {
+        public bool IsAlive { get; set; }
+        public object Age { get; set; }
+        public object LegCount { get; set; }
+        public object LandSeaAir { get; set; }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inheritance
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             // TODO Be sure to follow best practice when creating your classes
@@ -28,12 +30,53 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+            var myBird = new Bird();
+            myBird.WingColor = "yellow";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 2.5;
+
+            Console.WriteLine($"Alive:{true}");
+            Console.WriteLine($"Age: 0 years old");
+            Console.WriteLine($"It has 2 legs");
+            Console.WriteLine($"It lives by air");
+            Console.WriteLine();
+
+            var lizard = new Reptile();
+            lizard.IsColdBlooded = true;
+            lizard.IsScaly = true;
+            lizard.Habitat = "Swamp";
+            lizard.CanGrowTail = true;
+
+            Console.WriteLine($"Alive:{true}");
+            Console.WriteLine($"Age: 0 years old");
+            Console.WriteLine($"It has 4 legs");
+            Console.WriteLine($"It lives by land");
+
+     
+
+
+
+            var myAnimals = new Animal[] { };
+
+            foreach(var Animal in myAnimals) 
+            {
+                Console.WriteLine($"Alive:{Animal.IsAlive}");
+                Console.WriteLine($"Age:{Animal.Age} years old");
+                Console.WriteLine($"It has{Animal.LegCount}legs");
+                Console.WriteLine($"It lives by{Animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+
+
+
         }
     }
 }
